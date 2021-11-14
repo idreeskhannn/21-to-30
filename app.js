86d3441222d8240bca39b85237759b402b3a6ce3
muhamaad idrees <midreeskhan7276@gmail.com>
2021-11-14 11:49:01 -0800

    commit

---
+++ app.js
@@ -0,0 +1,222 @@
+
+
+               //  CHAPTER 21 TO 26  /////
+
+ // STRING METHODS
+ 
+ // question = 1 //
+ 
+ 
+ // var firstName = prompt("Enter Your FirstName");
+ // var lastName = prompt("Enter Your LastName");
+ // document.write(firstName + " " + lastName);
+ 
+ // question = 2 //
+ 
+ 
+ // var value = prompt("Enter any name");
+ // var value1 = names.length;
+ // document.write(value + " = "+" "+ value1);
+ 
+ // question = 3 //
+
+ // var value = "Pakistani";
+ // var index = value.indexOf("n");
+ // document.write("Index of 'n'" + " "+ index);
+ 
+ 
+ // question = 4 //
+ 
+ // var value = "Hell World";
+ // var value1 = value.lastIndexOf("l");
+ // document.write(" Last index of 'l'" + " "+ value1);
+ 
+ // qustion = 5 //
+ 
+ // var value = "Pakistani";
+ // var chart = value.charAt(3);
+ // document.write("Character at  index '3'" + " "+ chart);
+ 
+ // question = 6 //
+ 
+ // var firstName = prompt("Enter Your FirstName");
+ // var lastName = prompt("Enter Your LastName");
+ // var conc = firstName.concat(lastName);
+ // document.write(conc);
+ 
+ // question = 7 //
+ 
+ // var value = "Hyderbad";
+ // var rep = value.replace("Hyder","Islam");
+ // document.write(rep);
+ 
+ //  questtion  = 8 //
+ 
+ // var value = "Ali and Sami are best friends. They play cricket and football together.";
+ // var rep = value.replace(/and/g ,"&");
+ // document.write(rep);
+  
+ 
+ // question  = 9 //
+ 
+ // var value = "472";
+ // var hu = Number(value);
+ // document.write(hu);
+ 
+ 
+ // question = 10 //
+ 
+ // var value = prompt("Enter Your Name").toUpperCase();
+ // document.write(value)
+ 
+ // question = 11 //
+ 
+ // var user =prompt("Enter Your Name");
+ 
+ // var name = user[0].toUpperCase();
+ // var othername = user.slice(1).toLowerCase();
+ // document.write(name+othername);
+ 
+ 
+ // question  = 12 //
+ 
+ 
+ // var num = 35.36;
+ // var strin =num.toString().replace(".", ""); 
+ // document.write(strin)
+ 
+ 
+ // question = 13 //
+ 
+//  var user = prompt("Enter special name").trim;
+//  var arr =["@","!",",",".","$"];
+//  for(var a =0; a<arr.length ; a++ ){
+//      if(user === arr[a]){
+//          alert("invalid")
+
+//      }
+//  }
+//  var user = prompt("enter ur name").trim();
+//  for (var i = 0 ; i <user.length ; i++){
+//  if(user.charAt(i) === "@"){
+//      alert("invalid")
+//  }
+//  else if(user.charAt(i) === "$"){
+//      alert("invalid")
+//  }
+//  else if(user.charAt(i) === "."){
+//     alert("invalid")
+// }else if(user.charAt(i) === "#"){
+//     alert("invalid")
+// }
+
+//  }
+ // QUESTION = 14 //
+ 
+ // var user = prompt("enter item").toLowerCase()
+ // // var isMatch= "no"
+ // var items = ["cake " ,"coca cola " , "juice" , "biscuit"]
+ //  for (var i = 0 ; i<items.length ; i++){
+ //      if(user === items[i]){
+ //          alert(user + " is available at index " + i +" in our bakery ")
+ //     isMatch= "yes"
+ //         }
+ //  }if (isMatch == "no"){
+ //      alert("not available")
+ //  }
+ // 
+ 
+ // QUESTION = 15 //
+ 
+ 
+ // QUESTION = 16 //
+ 
+ 
+ 
+ 
+ 
+ // QUESTION = 17 //
+ 
+ 
+ // var names = "Pakistan";
+ // var name1=names.slice(-1);
+ // document.write(name1);
+ 
+ 
+ // QUESTION = 18 //
+ 
+ 
+
+
+
+///// chapter no 26 to 30  ///////////////
+
+
+//   question = 1 
+
+// var user = +prompt("enter a positive number ")
+
+// var round = Math.round(user)
+// var floor = Math.floor(user)
+// var ceil = Math.ceil(user)
+
+// document.write("user value = "+ user + "<br>" +"Round Value =" +  round + "<br>"+ " Floor value =  " +floor + "<br>" +" Ceil Value  = " + ceil)
+
+
+// //   question = 2
+// var user = +prompt("enter a negative number ")
+
+// var round = Math.round(user)
+// var floor = Math.floor(user)
+// var ceil = Math.ceil(user)
+
+// document.write("user value = "+ user + "<br>" +"Round Value =" +  round + "<br>"+ " Floor value =  " +floor + "<br>" +" Ceil Value  = " + ceil)
+
+
+// question = 3
+
+
+// var abs =+prompt("enter a number")
+
+// var uAbs = Math.abs(abs)
+// document.write("absolute number is : " + uAbs)
+
+// question = 4
+
+// var dice = +prompt("enter dice number")
+// var dicerol = Math.floor(Math.random(dice)*6 +1)
+// document.write("Random dice value : " + dicerol )
+
+
+// question = 5
+
+// var user = prompt("head")
+// var user1= prompt("tail")
+
+// var ran = Math.floor(Math.random() *2 +1)
+// if(ran == "1"){
+//     document.write("head win " + user)
+
+// }
+// else if(ran == 2){
+//   document.write("tail win" + user1)
+// }
+
+// // question = 6
+
+// var ran = Math.floor(Math.random()*100 +1)
+// document.write("Random number between 1 to 100 : " + ran )
+
+// question = 7
+
+// var user = +prompt("enter your wieght ")
+
+// var num = Number(user)
+// document.write("your wieght is :  " + num)
+
+
+
+// question = 8
+
+
+// CONFUSE
